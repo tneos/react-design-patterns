ce6707d1d8ed6cda6dd67b12c0bebc2a125d6f3e
Thomas Neos <tneos@hotmail.com>
2025-12-07 17:09:07 +0000

    Add Modal component

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import {RegularList} from "./components/lists/Regular";
 import authors from "./data/authors";
 import books from "./data/books";
 import {NumberedList} from "./components/lists/Numbered";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <>
-      <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem} />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
+      {/* <RegularList items={authors} sourceName={"author"} ItemComponent={SmallAuthorListItem} />
       <NumberedList items={authors} sourceName={"author"} ItemComponent={LargeAuthorListItem} />
       <RegularList items={books} sourceName={"book"} ItemComponent={SmallBookListItem} />
-      <NumberedList items={books} sourceName={"book"} ItemComponent={LargeBookListItem} />
+      <NumberedList items={books} sourceName={"book"} ItemComponent={LargeBookListItem} /> */}
     </>
   );
 }
